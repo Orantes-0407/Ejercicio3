57248f1bb3a298d5ae2216957c605eee6ef2c736
Melany Rosmery Pérez Orantes <mperezo37@miumg.edu.gt>
2025-02-15 10:37:42 -0600

    Add files via upload

---
+++ ejercicio1/Ejercicio 3}.cs
@@ -0,0 +1,57 @@
+﻿int num1;
+int num2;
+void saludar()
+
+    
+{
+    Console.Write("Hola!");
+    
+}
+
+void menu()
+{
+    Console.WriteLine("Ingrese a los menu del 1 al 4");
+    int opcion;
+   
+    opcion = int.Parse(Console.ReadLine());
+    switch (opcion)
+    
+    {
+        case 1: Console.WriteLine("1. Suma");
+           
+           
+
+            Console.WriteLine("Ingrese el primer numero");
+            Console.ReadLine();
+            Console.WriteLine("Ingrese el segundo numero");
+            int suma;
+            {
+                
+                  
+                
+            }
+         
+                
+
+
+            break;
+            case 2: Console.WriteLine("2.Resta");
+            break;
+            case 3: Console.WriteLine("3.Multiplicacion");
+            break;
+            case 4: Console.WriteLine("4.Division");
+            break;
+    }
+    
+        
+    
+
+
+
+
+    
+   
+
+
+
+
---
+++ ejercicio1/Program.cs
@@ -0,0 +1,38 @@
+﻿void saludar()
+{
+    Console.Write("Hola!");
+}
+
+int suma (int n1, int n2)
+{
+    int resultado = 0;
+    resultado = n1+ n2;
+    return resultado;
+}
+saludar();
+int multi(int n3, int n4)
+   
+{
+    
+    int resultado = 0;
+    resultado = n3 * n4;
+    return resultado;
+    
+   
+    
+   
+
+}
+int param1;
+int param2;
+param1 = 10;
+param2 = 20;
+int respuesta = suma(5, 9);
+int respuestaMul = multi(param1, param2);
+respuesta = multi(5, 9);
+Console.WriteLine($"La suma es:{respuesta}");
+
+
+Console.WriteLine($"La multiplicacion es:{respuesta}");
+
+
